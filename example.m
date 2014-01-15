931c8fccc056314ac2ec1eee53bd717d2de50239
puyal <laurine.puyal@yahoo.fr>
2014-01-15 15:56:09 +0100

    exemple.M

--- example.m
+++ example.m
@@ -1,10 +1,17 @@
 #import <stdio.h>
 
+int test (int a, int b){
+	int res=0;
+	res=a+b;
+	return res;
+}
+
 int main( int argc, const char *argv[] ) {
 	int numerator;
-	bool f;
-	double dou;
-	int cooper;
-	numerator = 10 + 6*2*3;
+	BOOL p;
+	float cooper;
+	numerator = 10 + 6;
+	if(numerator==16){
+	  cooper=2.1;};
     return 0;
 }
